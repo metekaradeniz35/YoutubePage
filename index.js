b92a860f8ec2d3fdff3d7bc8144eb0528f42e27f
metekaradeniz35 <82820834+metekaradeniz35@users.noreply.github.com>
2023-09-30 12:11:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("b24").onclick = function() {
+    document.getElementById("shortstitle").style.visibility = "hidden";
+    document.getElementById("shortslabel").style.visibility = "hidden";
+    document.getElementById("shortsvideos").style.visibility = "hidden";
+}
+
+document.getElementById("b32").onclick = function() {
+    location.reload();
+}
